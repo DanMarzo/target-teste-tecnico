f48633d51ae42474182e4943cb0bdd4ee872f974
DanMarzo <marzogildan@gmail.com>
2024-09-03 20:59:06 -0300

    Finalizando Questao 2 (Validar se pertence a sequencia de Fibonnaci)

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿namespace FibonacciApp;
+
+public class Fibonacci
+{
+    public Fibonacci(int numero)
+    {
+        this.Numero = numero;
+    }
+
+    public int Numero { get; set; }
+
+    public bool FibonacciValido() => this.RaizQuadradaPerfeita();
+    private bool RaizQuadradaPerfeita()
+    {
+        //Calcula a base onde sera subtraido ou adicionado valores para validacao;
+        int potencia = 5 * this.Numero * this.Numero;
+        
+        int potenciaNeg = potencia - 4;
+        int potenciaPos = potencia + 4;
+
+        int raizQuadNeg = (int)Math.Sqrt(potenciaNeg);
+        int raizQuadPos = (int)Math.Sqrt(potenciaPos);
+
+        return ((raizQuadNeg * raizQuadNeg) == potenciaNeg) || ((raizQuadPos * raizQuadPos) == potenciaPos);
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using FibonacciApp;
+
+Console.WriteLine("Digite um número:");
+var numEntrada = Console.ReadLine();
+
+try
+{
+
+    if (int.TryParse(numEntrada, out int numeroValido))
+    {
+        var fibonacci = new Fibonacci(numeroValido);
+
+        var mensagem = fibonacci.FibonacciValido()
+            ? $"Número {fibonacci.Numero} pertence."
+            : $"Número {fibonacci.Numero} não pertence.";
+        Console.WriteLine(mensagem);
+        return;
+    }
+    Console.WriteLine($"Valor {numEntrada} não é um numero.");
+    return;
+}
+catch (Exception ex) 
+{
+    Console.WriteLine($"Erro inesperado.");
+    Console.WriteLine(ex.Message);
+    return;
+}
