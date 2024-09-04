4d94554b5fdd2e943ddcded5d1cf2915057eeabb
DanMarzo <marzogildan@gmail.com>
2024-09-03 22:14:04 -0300

    Incluindo inversor de texto

---
+++ InverterTexto_5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite um texto para inverter:");
+var entrada = Console.ReadLine();
+
+if (string.IsNullOrEmpty(entrada))
+{
+    Console.WriteLine("Nenhum texto fornecido.");
+    return;
+}
+var textoInvertido = string.Empty;
+
+var qtdeCaracteres = entrada.Count();
+
+for (int i = 1; i <= qtdeCaracteres; i++)
+{
+    textoInvertido += entrada[qtdeCaracteres - i];
+}
+Console.WriteLine(textoInvertido);
